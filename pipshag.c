7ef9e80100bbca1eb7ec3df0f79e77ed2b5317e1
Pipshag <carlchristian.ljunggren@gmail.com>
2023-02-06 14:06:33 +0100

    Remove Retroshift, keep Auto for non-alphas

--- pipshag.c
+++ pipshag.c
@@ -1,14 +1,4 @@
 /*
  * Functions and code
  */
-#include QMK_KEYBOARD_H
-
-#ifdef AUTO_SHIFT_ENABLED
-// Retro shift requirement
-bool get_custom_auto_shifted_key(uint16_t keycode, keyrecord_t *record) {
-    if (IS_RETRO(keycode)) {
-        return true;
-    }
-    return false;
-}
-#endif
+/* #include QMK_KEYBOARD_H */
